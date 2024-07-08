367a68bf363136d73de148ff2a8748ab144f7c55
Henderson <hendersonsousa5@gmail.com>
2024-07-08 13:17:46 -0300

    modified:   index.html 	modified:   script.js 	modified:   styles.css

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ document.getElementById('hamburger-menu').addEventListener('click', function () 
     navMenu.classList.toggle('active');
     headerImg.classList.toggle('nav-active');
 });
+
+document.addEventListener("DOMContentLoaded", function() {  const animatedElements = document.querySelectorAll('.animated-element');
+    const observer = new IntersectionObserver(entries => {    entries.forEach(entry => {      if (entry.isIntersecting) {        entry.target.classList.add('is-visible');      }    });  });
+    animatedElements.forEach(element => {    observer.observe(element);  });});
