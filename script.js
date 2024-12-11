643003ee1bc04fd3c2e6c0dd5f07697cb7cf9d71
Henderson <hendersonsousa5@gmail.com>
2024-12-11 09:41:27 -0300

    menu redesign

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburgerMenu = document.getElementById("hamburger-menu");
+    const navMenu = document.getElementById("nav-menu");
 
-document.getElementById('hamburger-menu').addEventListener('click', function () {
-    var navMenu = document.querySelector('nav');
-    var headerImg = document.querySelector('header img');
-    navMenu.classList.toggle('active');
-    headerImg.classList.toggle('nav-active');
+    hamburgerMenu.addEventListener("click", () => {
+        hamburgerMenu.classList.toggle("active");
+        navMenu.classList.toggle("active");
+    });
 });
+
 
 document.addEventListener("DOMContentLoaded", function() {  const animatedElements = document.querySelectorAll('.animated-element');
     const observer = new IntersectionObserver(entries => {    entries.forEach(entry => {      if (entry.isIntersecting) {        entry.target.classList.add('is-visible');      }    });  });
